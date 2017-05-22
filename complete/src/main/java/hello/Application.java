cd6179e87b47281a2830dfe112e9acda81099a88
vad0009 <vitor.desiderio@walmart.com>
2017-05-22 12:49:57 -0300

    Corrigindo o Log4j

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -1,14 +1,15 @@
 package hello;
 
-import org.apache.log4j.Logger;
+
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
-	final static Logger logger = Logger.getLogger(Application.class);
+	
     public static void main(String[] args) {
-    	logger.info("Testando");
-        //SpringApplication.run(Application.class, args);
+    	
+        SpringApplication.run(Application.class, args);
     }
    
 }
