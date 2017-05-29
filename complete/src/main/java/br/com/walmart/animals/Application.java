7a45b8460dbe52bdb4df988996420507d2cfb79c
vad0009 <vitor.desiderio@walmart.com>
2017-05-29 14:34:14 -0300

    Update Project

---
+++ complete/src/main/java/br/com/walmart/animals/Application.java
@@ -0,0 +1,16 @@
+package br.com.walmart.animals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+@SpringBootApplication
+@EnableMongoRepositories("br.com.walmart.animals.repository")
+@ComponentScan({"br.com.walmart.animals.service"})
+public class Application {
+
+  public static void main(String[] args) {	
+     SpringApplication.run(Application.class, args);
+} 
+}
