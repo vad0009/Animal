7a7b4bab89a431d5023bc4b1634db97bb58f059a
vad0009 <vitor.desiderio@walmart.com>
2017-06-07 12:34:51 -0300

    Update README.md

---
+++ complete/src/main/java/br/com/walmart/animals/config/RedisConfig.java
@@ -0,0 +1,45 @@
+package br.com.walmart.animals.config;
+
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@Configuration
+@EnableCaching
+public class RedisConfig extends CachingConfigurerSupport {
+
+	//Conectando com o servidor do redis
+  @Bean
+  public JedisConnectionFactory redisConnectionFactory() {
+    JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+
+    // Defaults
+    redisConnectionFactory.setHostName("127.0.0.1");
+    redisConnectionFactory.setPort(6379);
+    return redisConnectionFactory;
+  }
+  
+  //Setando o template no servidor do redis
+  @Bean
+  public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+    RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+    redisTemplate.setConnectionFactory(cf);
+    return redisTemplate;
+  }
+
+  @Bean
+  public CacheManager cacheManager(RedisTemplate<String, String> redisTemplate) {
+    RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+
+    // Numeros de segundos antes de expirar. Por defaults o numero de segundos é (0), ou seja ilimitado.
+    cacheManager.setDefaultExpiration(300);
+    return cacheManager;
+  }
+
+}
